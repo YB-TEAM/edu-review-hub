55b1d45714b6bc0d6ff8d31735447e4fd8189a1e
bone204 <truongbmt4@gmail.com>
2025-07-03 11:20:40 +0700

    refactor: update ProfileModel and ProfileEntity to include additional fields, modify API URLs, and adjust UI components for enhanced user profile management

--- mobile/edu_review_mobile/lib/features/user_profile/domain/entities/profile.dart
+++ mobile/edu_review_mobile/lib/features/user_profile/domain/entities/profile.dart
@@ -1,15 +1,47 @@
 class ProfileEntity {
-  final String email;
-  final String userName;
-  final String? phoneNumber;
-  final String? birthday;
+  final BigInt userID;
+  final String? bio;
+  final DateTime? birthday;
+  final String? address;
+  final int? graduationYear;
+  final bool isStudent;
+  final DateTime createAt;
+  final DateTime updatedAt;
+  final String? firstName;
+  final String? lastName;
+  final String? displayName;
+  final String? avatarUrl;
+  final String? coverImageUrl;
   final String? gender;
+  final String? country;
+  final String? city;
+  final String timeZone;
+  final String language;
+  final String? universityName;
+  final String? major;
+  final String? studentId;
 
   ProfileEntity({
-    required this.email,
-    required this.userName,
-    this.phoneNumber,
+    required this.userID,
+    this.bio,
     this.birthday,
+    this.address,
+    this.graduationYear,
+    required this.isStudent,
+    required this.createAt,
+    required this.updatedAt,
+    this.firstName,
+    this.lastName,
+    this.displayName,
+    this.avatarUrl,
+    this.coverImageUrl,
     this.gender,
+    this.country,
+    this.city,
+    required this.timeZone,
+    required this.language,
+    this.universityName,
+    this.major,
+    this.studentId,
   });
 }
