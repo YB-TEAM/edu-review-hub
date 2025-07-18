9377ed30acec163f853a5c4f4800606caf777741
bone204 <truongbmt4@gmail.com>
2025-07-18 14:00:20 +0700

    update: refresh token logic

--- mobile/edu_review_mobile/lib/common/constants/api_urls.dart
+++ mobile/edu_review_mobile/lib/common/constants/api_urls.dart
@@ -3,6 +3,7 @@ class ApiUrls {
   //Authentication URLs
   static const register = "${baseURL}auth/register";
   static const login = "${baseURL}auth/login";
+  static const refreshToken = "${baseURL}auth/refresh";
 
   //User Profile URLs
   static const userProfile = "${baseURL}profile/me";
