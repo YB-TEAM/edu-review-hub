9a65013c07a0b19545a426b1808e539c86f1d95f
bone204 <truongbmt4@gmail.com>
2025-07-29 10:02:41 +0700

    feat: OTP input UI

--- mobile/edu_review_mobile/lib/common/constants/api_urls.dart
+++ mobile/edu_review_mobile/lib/common/constants/api_urls.dart
@@ -1,5 +1,5 @@
 class ApiUrls {
-  static const baseURL = "http://192.168.99.113:3000/api/v1/";
+  static const baseURL = "http://192.168.57.123:3000/api/v1/";
   // static const baseURL = "http://localhost:3000/api/v1/";
   //Authentication URLs
   static const register = "${baseURL}auth/register";
