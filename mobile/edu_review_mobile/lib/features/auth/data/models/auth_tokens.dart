7fc0ff23f63f70cbdd18da046853595eb2425457
bone204 <truongbmt4@gmail.com>
2025-07-30 21:19:59 +0700

    update: sign in, sign up call api

--- mobile/edu_review_mobile/lib/features/auth/data/models/auth_tokens.dart
+++ mobile/edu_review_mobile/lib/features/auth/data/models/auth_tokens.dart
@@ -1,14 +1,18 @@
+import 'package:edu_review_mobile/features/auth/data/models/user.dart';
+
 class AuthTokenModel {
   final String accessToken;
   final String refreshToken;
   final String tokenType;
   final int expiresIn;
+  final UserModel user;
 
   AuthTokenModel({
     required this.accessToken,
     required this.refreshToken,
     required this.tokenType,
     required this.expiresIn,
+    required this.user,
   });
 
   factory AuthTokenModel.fromJson(Map<String, dynamic> json) {
@@ -17,6 +21,7 @@ class AuthTokenModel {
       refreshToken: json['refreshToken'],
       tokenType: json['tokenType'],
       expiresIn: json['expiresIn'],
+      user: UserModel.fromJson(json['user']),
     );
   }
 }
